67666b87a9b9a066a1eda71492ddd3c5f7323c53
Derrek Young <derrek@derrekyoung.com>
2016-09-05 10:45:18 -0400

    Validate hosts via ping. Added logging utils

--- remote-agent-install.sh
+++ remote-agent-install.sh
@@ -14,35 +14,37 @@
 #
 ################################################################################
 
-####### Connection Information
+####### Connection Information #######
 # Your remote hosts in the form of username@hostname
-REMOTE_HOSTS="ubuntu@demosim"
-# REMOTE_HOSTS="ubuntu@server1,appd@server2,jsmith@server3"
+declare -a REMOTE_HOSTS=("ubuntu@demosim" \
+"foobar@google.com")
 
 # You must provide password or SSH key but **not both** at the same time. http://docs.fabfile.org/en/1.12/usage/execution.html#password-management
 REMOTE_PASSWORD_PROMPT=true
 # REMOTE_SSH_KEY="./archives/my-key.pem"
 
 
-####### AppDyanamics Information
+####### AppDyanamics Information #######
 # Where to install AppDynamics
 REMOTE_APPD_HOME="/opt/AppDynamics/"
 
-
+# Flag to toggle debug logging. Values= true|false
+DEBUG_LOGS=true
 
 ################################################################################
 
 main() {
-    validate-input "$@"
-
     local archive=$1 # Agent archive name
     declare CREDENTIALS="" # Global var to hold credentials
 
-    validate-dependencies
+    validate-input "$@"
+    validate-appd-home
+    validate-hosts ${REMOTE_HOSTS[@]}
+    validate-and-build-credentials
 
     startDate=$(date '+%Y-%m-%d %H:%M:%S')
     SECONDS=0
-    echo -e "Started:  $startDate"
+    log-info "Started:  $startDate"
 
     # Call Python Fabric to do remote management
     fab -f remote-agent-install.py --hosts "$REMOTE_HOSTS" "$CREDENTIALS" deploy_agent:archive="$archive",appd_home_dir="$REMOTE_APPD_HOME"
@@ -52,26 +54,22 @@ main() {
 
     endTime=$(date '+%Y-%m-%d %H:%M:%S')
     duration=$SECONDS
-    echo -e "Finished: $endTime. Time elsapsed: $(($duration / 60)) min, $(($duration % 60)) sec"
+    log-info "Finished: $endTime. Time elsapsed: $(($duration / 60)) min, $(($duration % 60)) sec"
 }
 
-validate-dependencies() {
+validate-appd-home() {
     # Verify that REMOTE_APPD_HOME is set
     if [[ -z ${REMOTE_APPD_HOME+x} ]]; then
-        echo -e "ERROR:\n You must set the remote AppDyanmics home directory at the top of this script"
+        log-error "You must set the remote AppDyanmics home directory"
         exit 1
     fi
+}
 
-    # Verify that REMOTE_HOSTS is set
-    if [[ -z ${REMOTE_HOSTS+x} ]]; then
-        echo -e "ERROR:\n You must set the list of hosts at the top of this script"
-        exit 1
-    fi
-
+validate-and-build-credentials() {
     # Verify that we have either REMOTE_SSH_KEY or REMOTE_PASSWORD_PROMPT set
     if [[ ! -z ${REMOTE_SSH_KEY+x} ]]; then
         if [ ! -f "$REMOTE_SSH_KEY" ]; then
-            echo -e "ERROR:\n   File not found, $REMOTE_SSH_KEY"
+            log-error "File not found, $REMOTE_SSH_KEY"
             exit 1
         fi
 
@@ -81,9 +79,11 @@ validate-dependencies() {
          CREDENTIALS="--initial-password-prompt"
 
     else
-        echo -e "ERROR:\n   You must set the SSH key or password prompt at the top of this script"
+        log-error "You must set the SSH key or password prompt"
         exit 1
     fi
+
+    log-debug "CREDENTIALS=$CREDENTIALS"
 }
 
 validate-input() {
@@ -94,9 +94,61 @@ validate-input() {
     fi
 
     if [ ! -f "$1" ]; then
-        echo -e "ERROR:\n File not found, $1"
+        log-error "File not found, $1"
         exit 1
     fi
+}
+
+validate-hosts() {
+    local hosts=${REMOTE_HOSTS[@]}
+    # echo "All Hosts: ${REMOTE_HOSTS[@]}"
+
+    # Verify that REMOTE_HOSTS is set
+    if [[ -z ${REMOTE_HOSTS+x} ]]; then
+        log-error "You must define the list of hosts"
+        exit 1
+    fi
+
+    for host in "${REMOTE_HOSTS[@]}"
+    do
+        # echo "Testing $host"
+        validate-host $host
+    done
+}
+
+validate-host() {
+    local userHostCombo=$1
+
+    IFS='@' read username hostname <<< "$userHostCombo"
+    # echo "username=$username, hostname=$hostname"
+
+    ping-test $hostname
+}
+
+ping-test() {
+    local hostname=$1
+
+    count=$( ping -c 1 $hostname | grep icmp* | wc -l )
+    if [ $count -eq 0 ]; then
+      log-error "Unable to ping $hostname"
+      exit 1
+    else
+      log-info "Successful ping to $hostname"
+    fi
+}
+
+log-info() {
+    echo -e "INFO:  $1"
+}
+
+log-debug() {
+    if [ $DEBUG_LOGS = true ]; then
+        echo -e "DEBUG: $1"
+    fi
+}
+
+log-error() {
+    echo -e "ERROR: \n       $1"
 }
 
 main "$@"
